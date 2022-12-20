2e95bb7269d1667ea87074971697ac52ff2802ac
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2022-12-21 00:06:12 +0200

    var v 1

---
+++ java/src/variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String[] args) {
+        byte myByte = 115;
+        System.out.println(myByte);
+        short myShort = 7899;
+        int myInt = 2572522;
+        long myLong = 257927295254929254L;
+    }
+}
