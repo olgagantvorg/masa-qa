1cf75ae453a280d3625d0d941201fcb000d4d5d8
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2022-12-20 20:47:15 +0200

    First commit

--- java/src/task.java
+++ java/src/task.java
@@ -1,9 +1,9 @@
-public class hello {
+public class task {
     public static void main(String[] args) {
 
         System.out.println("Много лет размышлял я над жизнью земной");
         System.out.println("Непонятного нет для меня под луной.");
         System.out.println("Мне известно, что мне ничего не известно!");
         System.out.println("Вот последняя правда, открытая мной.");
-     }
+    }
 }
