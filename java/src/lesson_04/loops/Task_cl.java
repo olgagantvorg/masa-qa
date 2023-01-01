a34f7b1b058b5700fc21bdee451c665471dc78f9
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-01 20:58:26 +0200

    ц

--- java/src/lesson_04/loops/Task_cl.java
+++ java/src/lesson_04/loops/Task_cl.java
@@ -3,9 +3,11 @@ package lesson_04.loops;
 public class Task_cl {
     public static void main(String[] args) {
         int num = 0;
-        while (num <= 1000){
-            if num % 5 == 0;
-            System.out.println(num);
+        while (num <= 1000) {
+            if (num % 5 == 0) {
+                System.out.println("num = " + num);
+            }
+            num++;
         }
     }
 }
