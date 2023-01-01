eeb0fefcf9abcb570234b919dd32e14a1a258f3d
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-01 18:42:40 +0200

    loop

--- java/src/lesson_04/loops/ForLoop.java
+++ java/src/lesson_04/loops/ForLoop.java
@@ -2,6 +2,9 @@ package lesson_04.loops;
 
 public class ForLoop {
     public static void main(String[] args) {
+        for (int counter = 0;counter < 3; counter ++){
+            System.out.println("Counter = " + counter);
+        }
 
     }
 }
