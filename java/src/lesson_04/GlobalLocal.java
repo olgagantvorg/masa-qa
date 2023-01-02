fd5c9980f82cf9626dac3f28b0239d5a477bee6f
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-02 11:22:12 +0200

    GlobalLocal

--- java/src/lesson_04/GlobalLocal.java
+++ java/src/lesson_04/GlobalLocal.java
@@ -3,5 +3,12 @@ package lesson_04;
 public class GlobalLocal {
     public static void main(String[] args) {
         String out = "Outside";
+        System.out.println("out = " + out);
+        for (int i = 0; i >= 0; i--) {
+            out = "We have inside loop";
+            int a = 5;
+            System.out.println("a = " + a);
+        }
+        System.out.println("out = " + out);
     }
 }
