4db738633c4e3f5706a85441a3a73e2c8af82892
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-09 10:45:49 +0200

    0901

--- java/src/lesson_5/HW/Duplicate.java
+++ java/src/lesson_5/HW/Duplicate.java
@@ -1,17 +1,29 @@
 package lesson_5.HW;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Duplicate {
     public static void main(String[] args) {
-        ArrayList <Integer> number = new ArrayList<>();
+        ArrayList <Integer> numbers = new ArrayList<>();
         Random random = new Random();
+        HashMap<Integer, Integer> count = new HashMap<>();
         for (int i = 0; i < 100; i++) {
-            number.add(random.nextInt(50));
+            numbers.add(random.nextInt(50));
         }
-        System.out.println("number.size() = " + number.size());
+        for (int num : numbers) {
+            if (count.containsKey(num)){
+                count.put(num,(count.get(num) + 1));
+            }
+            else {
+                count.put(num,1);
+            }
+        }
+        System.out.println("Amount of unique numbers " + count.size() + " in the list.Here is their breakdown." + "Amount of not unique numbers is " + (numbers.size()- count.size()));
+        count.forEach((unicueNumber,number)-> System.out.println(unicueNumber + " number is in the list " + number + " times"));
+
     }
     }
 
