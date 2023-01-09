ecace126c3e1667a091f0dbdf7983fbac51dfb6c
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-09 22:30:26 +0200

    CW9

---
+++ java/src/lesson_6/MaxMinArray.java
@@ -0,0 +1,17 @@
+package lesson_6;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MaxMinArray {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> minMax = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            minMax.add(random.nextInt());
+        }
+
+    }
+
+            }
+
