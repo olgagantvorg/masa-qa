949facc24ecf45a1de32fcd7976c36d772fe12c3
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-01-12 17:02:20 +0200

    HW

---
+++ java/src/lesson_6/AreArraysEquel.java
@@ -0,0 +1,45 @@
+package lesson_6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class AreArraysEquel {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int array[] = new int[10];
+        int array2[] = new int[10];
+        System.out.println("Enter amount of numbers for first array ");
+        int amountArray = input.nextInt();
+        System.out.println("Enter amount of numbers for second array ");
+        int amountArray2 = input.nextInt();
+        for (int i = 0; i < amountArray; i++) {
+            System.out.println("Enter numbers for first array. You need to enter " + (amountArray - i) + " numbers");
+            array[i] = input.nextInt();
+            if (amountArray == array[i]) {
+                System.out.println("Thank you for adding numbers for first array");
+            }
+        }
+        for (int i = 0; i < amountArray2; i++) {
+            System.out.println("Enter numbers for second array. You need to enter " + (amountArray2 - i) + " numbers");
+            array2[i] = input.nextInt();
+            if (amountArray2 == array2[i]) {
+                System.out.println("Thank you for adding numbers for second array");
+            }
+        }
+        System.out.println("This two arrays are the same. Is it true?  " + arraysEqual(array, array2));
+        System.out.println("This two arrays are the same. Is it true?  " + arraysEqual2(array,array2));
+    }
+
+    public static boolean arraysEqual(int [] array1, int [] array2){
+        if (Arrays.equals(array1,array2)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean arraysEqual2(int [] array1, int [] array2){
+        return IntStream.range(0, array1.length).noneMatch(i -> array2[i] != array1[i]);
+        }
+    }
+
--- java/src/lesson_6/IsNumberExist.java
+++ java/src/lesson_6/IsNumberExist.java
@@ -1,11 +1,21 @@
 package lesson_6;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class IsNumberExist {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int myArray[] = {1,2,3,45};
+        int [] myArray = {1,2,3,4,5};
+        Integer [] myArray2 = {1,2,3,4,5};
+        System.out.println("Please enter your number ");
+        int myNumber = input.nextInt();
+        System.out.println("myCheckIn(myNumber,myArray) = " + myCheckIn(myNumber, myArray));
+        System.out.println("myCheckIn2(myNumber,myArray) = " + myCheckIn2(myNumber, myArray));
+        System.out.println("myCheckIn3(myNumber,myArray) = " + myCheckIn3(myNumber,myArray));
+
 
     }
 
@@ -20,13 +30,17 @@ public class IsNumberExist {
 
     }
     public static boolean myCheckIn2(int myValue, int[] array){
-        for (int number : array) {
-            if (myValue == number){
-                return true;
-            }
+        int findValue = IntStream.range(0,array.length).filter(i -> myValue == array[i]).findFirst().orElse(-1);
+        return findValue != -1 ? true :false;
+    }
 
-        }
-        return false;
+    public static boolean myCheckIn3(int myValue2, int[] array){
+//        List<Integer> list = Arrays.asList(array);
+//        System.out.println(list);
+        int foundedValue  = Arrays.asList(array).indexOf(myValue2);
+        return foundedValue != -1;
+    }
+
 
 
 }
